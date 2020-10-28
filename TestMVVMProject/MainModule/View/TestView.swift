76293bb41406a6266d9cdd47f3d5d99dfdbcd33c
Алексей Селиванов <a.selivanov@digital-lab.ru>
2020-10-28 14:32:23 +0300

    init commit

---
+++ TestMVVMProject/MainModule/View/TestView.swift
@@ -0,0 +1,21 @@
+//
+//  TestView.swift
+//  TestMVVMProject
+//
+//  Created by Алексей Селиванов on 28.10.2020.
+//  Copyright © 2020 Alexey Selivanov. All rights reserved.
+//
+
+import UIKit
+
+class TestView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TestMVVMProject/MainModule/View/TestViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  TestViewExtension.swift
+//  TestMVVMProject
+//
+//  Created by Алексей Селиванов on 28.10.2020.
+//  Copyright © 2020 Alexey Selivanov. All rights reserved.
+//
+
+import Foundation
