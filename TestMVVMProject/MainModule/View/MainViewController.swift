a3508a962fb63d3dd0ccc25517a2ef9fd3e660e1
Алексей Селиванов <a.selivanov@digital-lab.ru>
2020-10-28 14:34:01 +0300

    test mvvm

--- TestMVVMProject/MainModule/View/MainViewController.swift
+++ TestMVVMProject/MainModule/View/MainViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private var viewModel: MainViewModelProtocol!
+    private var testView: TestView!
 
     override func viewDidLoad() {
+        // hardcore
+        viewModel = MainViewModel()
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createView()
+        updateView()
+    }
+    
+    private func createView() {
+        testView = TestView()
+        testView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        testView.center = view.center
+        view.addSubview(testView)
+    }
+    
+    private func updateView() {
+        viewModel.updateViewData = { [weak self] viewData in
+            self?.testView.viewData = viewData
+        }
     }
 
-
+    @IBAction func startAction(_ sender: UIButton) {
+        viewModel.startFetch()
+    }
 }
 
--- TestMVVMProject/MainModule/View/TestView.swift
+++ TestMVVMProject/MainModule/View/TestView.swift
@@ -9,13 +9,48 @@
 import UIKit
 
 class TestView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var viewData: ViewData = .initial {
+        didSet {
+            setNeedsLayout()
+        }
     }
-    */
 
+    lazy var imageView = makeImageView()
+    lazy var activityIndicator = makeActivityIndicatorView()
+    lazy var titleLabel = makeTitleLabel()
+    lazy var descriptionLabel = makeDescriptionLabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        switch viewData {
+        case .initial:
+            update(viewData: nil, isHidden: true)
+            activityIndicator.isHidden = true
+            activityIndicator.stopAnimating()
+        case .loading(let loading):
+            update(viewData: loading, isHidden: false)
+            activityIndicator.isHidden = false
+            activityIndicator.startAnimating()
+        case .success(let success):
+            update(viewData: success, isHidden: false)
+            activityIndicator.isHidden = true
+            activityIndicator.stopAnimating()
+        case .faillure(let failure):
+            update(viewData: failure, isHidden: false)
+            activityIndicator.isHidden = true
+            activityIndicator.stopAnimating()
+        }
+    }
+    
+    func update(viewData: ViewData.Data?, isHidden: Bool) {
+        imageView.image = UIImage(named: viewData?.icon ?? "")
+        titleLabel.text = viewData?.title
+        descriptionLabel.text = viewData?.description
+        titleLabel.isHidden = isHidden
+        descriptionLabel.isHidden = isHidden
+        imageView.isHidden = isHidden
+    }
+    
 }
