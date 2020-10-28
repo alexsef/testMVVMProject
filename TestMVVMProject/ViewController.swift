274a7a402761474cd538efd81f8eb58a92bf709d
Алексей Селиванов <a.selivanov@digital-lab.ru>
2020-10-28 12:57:21 +0300

    Initial Commit

---
+++ TestMVVMProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestMVVMProject
+//
+//  Created by Алексей Селиванов on 28.10.2020.
+//  Copyright © 2020 Alexey Selivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
